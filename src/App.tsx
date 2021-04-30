d05dbcc9dc2523aa65507ecbd756bf93f898a368
Burt <2902978956@qq.com>
2021-04-30 13:17:43 +0800

    yarn eject, webpack 配置 alias

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import React, { Suspense, lazy } from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import { Switch, Route, HashRouter as Router } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Loading from './components/Loading';
+import Header from '@/components/Header';
+import Footer from '@/components/Footer';
+import Loading from '@/components/Loading';
 
 const Home = lazy(() => import('./pages/Home'));
-const History = lazy(() => import('./pages/History'))
-const About = lazy(() => import('./pages/About'))
+const History = lazy(() => import('./pages/History'));
+const About = lazy(() => import('./pages/About'));
 
 function App () {
   return (
     <div>
       <Header/>
       <Router>
-        <Suspense fallback={<Loading />}>
+        <Suspense fallback={<Loading/>}>
           <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/history" component={History} exact />
-            <Route path="/about" component={About} exact />
+            <Route path="/" component={Home} exact/>
+            <Route path="/history" component={History} exact/>
+            <Route path="/about" component={About} exact/>
           </Switch>
         </Suspense>
       </Router>
